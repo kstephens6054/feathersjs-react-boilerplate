fbe14cd3fb2d2f3a18cf0e48c905a37d09189913
Kurt Stephens <kstephens6054@gmail.com>
2020-11-07 15:46:10 -0500

    Set up proxy and new API server base URL

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-console */
+const express = require('@feathersjs/express');
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const baseURL = app.get('baseURL');
+
+const api = express().use(baseURL, app);
+const server = api.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  logger.info(
+    'Feathers application started on http://%s:%d%s',
+    app.get('host'),
+    port,
+    baseURL
+  )
 );
