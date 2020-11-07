7b339e423126aeb331f306cde731cd36cbac7bae
Kurt Stephens <kstephens6054@gmail.com>
2020-11-07 17:31:52 -0500

    Updated server to serve static content on build

--- server/src/api.js
+++ server/src/api.js
@@ -1,5 +1,3 @@
-const path = require('path');
-const favicon = require('serve-favicon');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -12,45 +10,43 @@ const socketio = require('@feathersjs/socketio');
 
 const middleware = require('./middleware');
 const services = require('./services');
-const appHooks = require('./app.hooks');
+const apiHooks = require('./api.hooks');
 const channels = require('./channels');
 
 const authentication = require('./authentication');
 
-const app = express(feathers());
+const api = express(feathers());
 
-// Load app configuration
-app.configure(configuration());
+// Load API configuration
+api.configure(configuration());
+
 // Enable security, CORS, compression, favicon and body parsing
-app.use(
+api.use(
   helmet({
     contentSecurityPolicy: false,
   })
 );
-app.use(cors());
-app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
-// Host the public folder
-app.use('/', express.static(app.get('public')));
+api.use(cors());
+api.use(compress());
+api.use(express.json());
+api.use(express.urlencoded({ extended: true }));
 
 // Set up Plugins and providers
-app.configure(express.rest());
-app.configure(socketio());
+api.configure(express.rest());
+api.configure(socketio());
 
 // Configure other middleware (see `middleware/index.js`)
-app.configure(middleware);
-app.configure(authentication);
+api.configure(middleware);
+api.configure(authentication);
 // Set up our services (see `services/index.js`)
-app.configure(services);
+api.configure(services);
 // Set up event channels (see channels.js)
-app.configure(channels);
+api.configure(channels);
 
 // Configure a middleware for 404s and the error handler
-app.use(express.notFound());
-app.use(express.errorHandler({ logger }));
+api.use(express.notFound());
+api.use(express.errorHandler({ logger }));
 
-app.hooks(appHooks);
+api.hooks(apiHooks);
 
-module.exports = app;
+module.exports = api;
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,28 @@
 /* eslint-disable no-console */
+const feathers = require('@feathersjs/feathers');
+const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const baseURL = app.get('baseURL');
+const path = require('path');
+const favicon = require('serve-favicon');
 
-const api = express().use(baseURL, app);
-const server = api.listen(port);
+const logger = require('./logger');
+const api = require('./api');
+
+const app = express(feathers());
+app.configure(configuration());
+
+const port = app.get('port');
+const apiBaseURL = app.get('apiBaseURL');
+
+// Host the public folder
+app.use('/', express.static(app.get('public')));
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+
+// Host the API
+app.use(apiBaseURL, api);
+
+// Run the server
+const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -14,9 +30,8 @@ process.on('unhandledRejection', (reason, p) =>
 
 server.on('listening', () =>
   logger.info(
-    'Feathers application started on http://%s:%d%s',
+    'Feathers application started on http://%s:%d',
     app.get('host'),
-    port,
-    baseURL
+    port
   )
 );
